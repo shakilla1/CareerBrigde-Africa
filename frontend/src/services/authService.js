d10e0abbf827514424142157438053f67e3664de
shakilla1 <s.uwamahoro1@alustudent.com>
2026-07-31 14:54:28 +0200

    fix routes

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,12 +2,12 @@ import api from "./api";
 import { clearAuthData } from "../utils/authStorage";
 
 export const registerUser = async (userData) => {
-  const response = await api.post("/auth/register", userData);
+  const response = await api.post("/api/auth/register", userData);
   return response.data;
 };
 
 export const loginUser = async (credentials) => {
-  const response = await api.post("/auth/login", credentials);
+  const response = await api.post("/api/auth/login", credentials);
   return response.data;
 };
 
